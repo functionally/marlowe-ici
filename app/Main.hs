d3f37936a11d5ff9ba664a42c4a64df5541c4dbc
Brian W Bush <brian.bush@iohk.io>
2022-04-30 11:00:56 -0600

    Create scaffold.

--- app/Main.hs
+++ app/Main.hs
@@ -1,7 +1,36 @@
+
+
 module Main (
   main
 ) where
 
 
-main :: IO
-main = pure ()
+import Cardano.Api
+import Control.Monad.Except
+import Language.Marlowe.CLI.Types
+import Language.Marlowe.ICI
+import System.Environment
+import System.Exit
+import System.IO
+
+
+main :: IO ()
+main =
+  do
+    [networkId, socketPath, continue, pointFile] <- getArgs
+    hSetBuffering stdout LineBuffering
+    hSetBuffering stderr LineBuffering
+    let
+      connection =
+        LocalNodeConnectInfo
+        {
+          localConsensusModeParams = CardanoModeParams $ EpochSlots 21600
+        , localNodeNetworkId       = Testnet $ NetworkMagic $ read networkId
+        , localNodeSocketPath      = socketPath
+        }
+    result <- runExceptT $ marloweChainIndex connection (read continue) (read pointFile)
+    case result of
+      Right ()      -> return ()
+      Left  message -> do
+                         hPutStrLn stderr $ unCliError message
+                         exitFailure
--- src/Language/Marlowe/ICI.hs
+++ src/Language/Marlowe/ICI.hs
@@ -1,7 +1,27 @@
+
+
+{-# LANGUAGE FlexibleContexts #-}
+
+
 module Language.Marlowe.ICI (
-  someFunc
+  marloweChainIndex
 ) where
 
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+import Cardano.Api
+import Control.Monad.Except
+import Language.Marlowe.CLI.Sync
+import Language.Marlowe.CLI.Sync.Types
+import Language.Marlowe.CLI.Types
+
+
+marloweChainIndex :: MonadError CliError m
+                 => MonadIO m
+                 => LocalNodeConnectInfo CardanoMode  -- ^ The local node connection.
+                 -> Bool                              -- ^ Whether to continue processing when the tip is reached.
+                 -> Maybe FilePath                    -- ^ The file to restore the chain point from and save it to.
+                 -> m ()                              -- ^ Action for watching for potential Marlowe transactions.
+marloweChainIndex connection continue pointFile =
+  watchMarloweWithPrinter connection continue pointFile
+    $ \e ->
+      print e

bd4cd3339b248f608a3ef65cbb9c36035a3bd33c
Brian W Bush <brian.bush@iohk.io>
2022-05-14 08:54:15 -0600

    added version check for marlowe address

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,6 @@
 
+{-# LANGUAGE OverloadedStrings #-}
+
 
 module Main (
   main
@@ -7,11 +9,15 @@ module Main (
 
 import Cardano.Api
 import Control.Monad.Except
+import Language.Marlowe.Client (defaultMarloweParams)
+import Language.Marlowe.CLI.Export (buildAddress)
 import Language.Marlowe.CLI.Types
 import Language.Marlowe.ICI
 import System.Environment
 import System.Exit
 import System.IO
+
+import qualified Data.Text.IO as T
 
 
 main :: IO ()
@@ -19,13 +25,17 @@ main =
   do
     hSetBuffering stdout LineBuffering
     hSetBuffering stderr LineBuffering
+    T.hPutStrLn stderr
+      $ "Version check: default Marlowe address = "
+      <> serialiseAddress (buildAddress defaultMarloweParams (Testnet $ NetworkMagic 1564) NoStakeAddress :: AddressInEra AlonzoEra)
     [networkId, socketPath, continue, pointFile] <- getArgs
     let
+      network = Testnet . NetworkMagic $ read networkId
       connection =
         LocalNodeConnectInfo
         {
           localConsensusModeParams = CardanoModeParams $ EpochSlots 21600
-        , localNodeNetworkId       = Testnet $ NetworkMagic $ read networkId
+        , localNodeNetworkId       = network
         , localNodeSocketPath      = socketPath
         }
     result <- runExceptT $ marloweChainIndex connection (read continue) (read pointFile)
